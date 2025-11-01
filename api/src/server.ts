4b4c80bc89b79a56262f5e8f99568d132c9273f3
Rafael Valverde <rafaverde@msn.com>
2025-10-31 21:41:00 -0300

    feat: Configure Fastify with Swagger and Zod.

--- api/src/server.ts
+++ api/src/server.ts
@@ -5,19 +5,53 @@ import { env } from "./env";
 import jwt from "@fastify/jwt";
 import { authRoutes } from "./routes/auth";
 import { usersRoutes } from "./routes/users";
+import swagger from "@fastify/swagger";
+import swaggerUi from "@fastify/swagger-ui";
+import {
+  ZodTypeProvider,
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
 
 // Inicializa o Fastify
 const app = Fastify({
   logger: true, // Habilita o logger do Fastify (ótimo para dev)
-});
+}).withTypeProvider<ZodTypeProvider>(); // "Ensina" o Fastify a entender os tipos do Zod.
 
-// Criação da Rota "Health Check"
-// Esta rota é essencial para sabermos se a API está online
-// e se consegue acessar o banco de dados.
+// Configura compiladores do Zod
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
 // Função para Iniciar o Servidor
 const start = async () => {
   try {
+    // Registra o Swagger
+    await app.register(swagger, {
+      openapi: {
+        info: {
+          title: "Macondo Link Manager API",
+          description: "API para o gerenciador de links da Macondo Propaganda.",
+          version: "1.0.0",
+        },
+        servers: [{ url: `http://localhost:${env.PORT}` }],
+      },
+      transform: ({ schema, url }) => {
+        // Ajusta os schemas para o formato OpenAPI
+        const { ...rest } = schema;
+        return { schema: rest, url };
+      },
+    });
+
+    // Registra o Swagger UI (Interface)
+    app.register(swaggerUi, {
+      routePrefix: "/docs",
+      staticCSP: true,
+      uiConfig: {
+        docExpansion: "list",
+        deepLinking: true,
+      },
+    });
+
     // Regista os plugins globais
     await app.register(jwt, {
       secret: env.JWT_SECRET,
@@ -32,6 +66,9 @@ const start = async () => {
     await app.register(authRoutes);
     await app.register(usersRoutes);
 
+    // Criação da Rota "Health Check"
+    // Esta rota é essencial para sabermos se a API está online
+    // e se consegue acessar o banco de dados
     await app.register(async (app) => {
       app.get("/health", async (request, reply) => {
         try {
