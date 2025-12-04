5eb4f1134fcde9c3f7df527a346e3378267ec48d
Rafael Valverde <rafaverde@msn.com>
2025-12-04 19:16:13 -0300

    feat: add sidebar component with mobile support and various UI enhancements
    - Added new dependencies for Radix UI components including Dialog, Separator, and Tooltip.
    - Created a new DashboardLayout component to wrap dashboard pages with a sidebar.
    - Implemented AppSidebar component with navigation links and icons.
    - Introduced UI components: Input, Separator, Sheet, Skeleton, and Tooltip for consistent styling.
    - Developed a custom hook  to manage mobile view state.
    - Added a new SVG icon for branding.

---
+++ web/src/app/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+import AppSidebar from "@/components/app-sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+    </SidebarProvider>
+  );
+}
---
+++ web/src/components/app-sidebar.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import * as React from "react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+} from "./ui/sidebar";
+import { usePathname } from "next/navigation";
+
+import { RiBarChartFill, RiLinksFill, RiWhatsappLine } from "@remixicon/react";
+import macondoLogo from "@/assets/macondo-logo.svg";
+import macondoIcon from "@/assets/macondo-icon.svg";
+import Image from "next/image";
+import Link from "next/link";
+
+const menuItems = [
+  { href: "/dashboard/links", label: "Meus Links", icon: RiLinksFill },
+  { href: "/dashboard", label: "Estatísticas", icon: RiBarChartFill },
+  {
+    href: "/",
+    label: "Whatsapp Link",
+    icon: RiWhatsappLine,
+    disabled: true,
+  },
+];
+
+export default function AppSidebar({
+  ...props
+}: React.ComponentProps<typeof Sidebar>) {
+  const pathname = usePathname();
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader className="h-20">
+        <div className="flex h-auto w-full max-w-[80%] items-center justify-center px-4 pt-6 group-data-[collapsible=icon]:hidden">
+          <Image
+            src={macondoLogo}
+            alt="Macondo Propaganda"
+            className="h-8 w-auto"
+          />
+        </div>
+
+        <div className="hidden items-center justify-center group-data-[collapsible=icon]:block">
+          <Image
+            src={macondoIcon}
+            alt="Macondo Propaganda"
+            className="size-8"
+          />
+        </div>
+      </SidebarHeader>
+
+      <SidebarContent>
+        <SidebarMenu className="p-2">
+          {menuItems.map((item, index) => {
+            const isActive = pathname === item.href;
+
+            return (
+              <SidebarMenuItem key={index + item.href}>
+                <SidebarMenuButton
+                  size="lg"
+                  asChild
+                  isActive={isActive}
+                  tooltip={item.label}
+                  className="data-[active=true]:border-macondo-gray-300 hover:bg-macondo-gray-100 p-4 data-[active=true]:border"
+                >
+                  <Link
+                    href={item.href}
+                    className="group-data-[collapsible=icon]:flex group-data-[collapsible=icon]:justify-center"
+                  >
+                    <item.icon className="text-primary size-5!" />
+                    <span className="group-data-[collapsible=icon]:hidden">
+                      {item.label}
+                    </span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            );
+          })}
+        </SidebarMenu>
+      </SidebarContent>
+
+      <SidebarRail />
+    </Sidebar>
+  );
+}
