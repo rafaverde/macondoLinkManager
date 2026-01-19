5a235bfd2ecae9dbef4ed153f15dbe8bf92e4b48
Rafael Valverde <rafaverde@msn.com>
2026-01-19 10:36:32 -0300

    fix(api): fix zod env.ts schema on base url variable

--- api/src/env.ts
+++ api/src/env.ts
@@ -8,7 +8,7 @@ const envSchema = z.object({
   GOOGLE_CLIENT_SECRET: z.string(),
   JWT_SECRET: z.string(),
   FRONTEND_URL: z.url(),
-  BASE_URL: process.env.BASE_URL!,
+  BASE_URL: z.url(),
   PORT: z.coerce.number().default(3333),
 });
 
