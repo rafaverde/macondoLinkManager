07d452eb609a883e188baa13ad9df519e0e2b24b
Rafael Valverde <rafaverde@msn.com>
2026-01-19 17:47:35 -0300

    fix(dashboard): clean middleware

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -1,53 +1,25 @@
 import { NextResponse, type NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  // Tenta pegar o token de autenticação
-  const token = request.cookies.get("macondo.token")?.value;
-  // Pega url para tratar erros de login
   const { pathname, searchParams } = request.nextUrl;
 
-  // Constate se há erro no url
   const authError = searchParams.get("error");
 
-  // Define quais rotas são protegidas
-  const isDashboardRoute = request.nextUrl.pathname.startsWith("/dashboard");
-  const isLoginRoute = request.nextUrl.pathname === "/";
-  const isAuthCallbackRoute = pathname.startsWith("/auth/callback");
+  const isLoginRoute = pathname === "/";
 
-  //Sempre libera o callback de auth (token ainda NÃO existe aqui)
-  if (isAuthCallbackRoute) {
-    return NextResponse.next();
-  }
-
-  // Sempre permite a home quando houver erro de auth
+  // Sempre permite login com erro
   if (isLoginRoute && authError) {
     return NextResponse.next();
   }
 
-  // Caso tente acessar Dashboard sem token > Redireciona para login
-  if (isDashboardRoute && !token) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+  /**
+   * 🔴 NÃO checar cookie aqui
+   * 🔴 NÃO redirecionar baseado em token
+   */
 
-  // Caso tente acessar login COM token > Redireciona para Dashboard
-  if (isLoginRoute && token && !authError) {
-    return NextResponse.redirect(new URL("/dashboard", request.url));
-  }
-
-  // Se nada disse acontecer, passa.
   return NextResponse.next();
 }
 
-// Configura as rotas em que o middleware deve rodar
 export const config = {
-  matcher: [
-    /*
-     * Corresponde a todos os caminhos de solicitação, exceto:
-     * - api (rotas de API internas do Next)
-     * - _next/static (arquivos estáticos)
-     * - _next/image (arquivos de otimização de imagem)
-     * - favicon.ico (ícone do navegador)
-     */
-    "/((?!api|_next/static|_next/image|favicon.ico).*)",
-  ],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
