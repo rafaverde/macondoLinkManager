285de27d788320de5f05dd839bb2ca00b27d1c06
Rafael Valverde <rafaverde@msn.com>
2026-01-27 16:03:23 -0300

    feat(web): add clients management page with table layout and actions buttons

---
+++ web/src/app/dashboard/clients/page.tsx
@@ -0,0 +1,72 @@
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { RiAddLine, RiDeleteBinLine, RiPencilLine } from "@remixicon/react";
+
+export default function ClientsPage() {
+  const isLoading = false;
+
+  return (
+    <>
+      <div className="flex items-center justify-between border-b pb-8">
+        <h2 className="text-4xl font-bold">Clientes</h2>
+
+        <Button size="lg">
+          Novo Cliente
+          <RiAddLine />
+        </Button>
+      </div>
+
+      {/* Table */}
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Nome</TableHead>
+            <TableHead>Criado em</TableHead>
+            <TableHead className="text-right">Ações</TableHead>
+          </TableRow>
+        </TableHeader>
+
+        <TableBody>
+          {isLoading &&
+            Array.from({ length: 8 }).map((item, i) => (
+              <TableRow key={i}>
+                <TableCell>
+                  <Skeleton className="bg-muted-foreground/20 h-8 w-full" />
+                </TableCell>
+                <TableCell>
+                  <Skeleton className="bg-muted-foreground/20 h-8 w-full" />
+                </TableCell>
+                <TableCell>
+                  <Skeleton className="bg-muted-foreground/20 h-8 w-full" />
+                </TableCell>
+              </TableRow>
+            ))}
+
+          {Array.from({ length: 8 }).map((item, i) => (
+            <TableRow key={i}>
+              <TableCell>Macondo Propaganda Cliente [{i + 1}]</TableCell>
+              <TableCell>20/01/2026</TableCell>
+              <TableCell className="space-x-2 text-right">
+                <Button size="icon" variant="outline">
+                  <RiPencilLine />
+                </Button>
+
+                <Button size="icon" variant="destructive">
+                  <RiDeleteBinLine />
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
+  );
+}
