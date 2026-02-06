ff3fb137ae0006875f4ac25e93025cbac8fea2e0
Rafael Valverde <rafaverde@msn.com>
2026-02-06 15:40:33 -0300

    chore: introduce read models for clients and campaigns listings

---
+++ api/src/http/read-models/campaign-list.item.ts
@@ -0,0 +1,8 @@
+export interface CampaignsListItem {
+  id: string;
+  name: string;
+  clientId: string;
+  clientName: string;
+  linksCount: number;
+  createdAt: Date;
+}
---
+++ api/src/http/read-models/client-list-item.ts
@@ -0,0 +1,7 @@
+export interface ClientListItem {
+  id: string;
+  name: string;
+  campaignsCount: number;
+  linksCount: number;
+  createdAt: Date;
+}
