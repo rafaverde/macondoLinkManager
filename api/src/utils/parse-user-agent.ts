5ee983761219ddf1e48a3312c54a01a75113c774
Rafael Valverde <rafaverde@msn.com>
2026-02-06 15:20:34 -0300

    fix: code lint adjustment

--- api/src/utils/parse-user-agent.ts
+++ api/src/utils/parse-user-agent.ts
@@ -4,19 +4,14 @@ export function parseBrowserName(ua?: string | null): string {
   const agent = ua.toLowerCase();
 
   if (agent.includes("edg")) return "Edge";
-
   if (agent.includes("chrome") && agent.includes("mobile")) {
     return "Chrome Mobile";
   }
-
   if (agent.includes("chrome")) return "Chrome";
-
   if (agent.includes("firefox")) return "Firefox";
-
   if (agent.includes("safari") && agent.includes("mobile")) {
     return "Safari iOS";
   }
-
   if (agent.includes("safari")) return "Safari";
 
   return "Outros";
